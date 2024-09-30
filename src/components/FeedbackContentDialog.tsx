7b48d30b8e13e21e6c754e8c463649cabb860e3e
Aaron Rackley <aaron.rackley@tribalworldwide.co.uk>
2024-09-30 08:05:18 +0100

    additional styling

--- src/components/FeedbackContentDialog.tsx
+++ src/components/FeedbackContentDialog.tsx
@@ -40,17 +40,25 @@ const FeedbackContentDialog = (props: FeedbackContentDialogProps) => {
             }}
           >
             <div class="flex items-center justify-between p-[33px] pb-[24px] rounded-t-[16px]">
-              <span class="whitespace-pre-wrap max-w-full text-[20px]" style={{
-                'font-weight': 300, 
-                'font-family': 'vw-text'
-              }}>Provide additional feedback</span>
+              <span
+                class="whitespace-pre-wrap max-w-full text-[20px]"
+                style={{
+                  'font-weight': 300,
+                  'font-family': 'vw-text',
+                }}
+              >
+                Provide additional feedback
+              </span>
               <button
                 class="p-1 ml-auto bg-transparent border-0 text-black float-right text-xl leading-none font-semibold outline-none focus:outline-none"
                 type="button"
                 onClick={onClose}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                  <path d="M19.5831 5.441L18.6411 4.5L12.056 11.1067L5.441 4.5L4.5 5.441L11.115 12.0477L4.51579 18.6215L5.45679 19.5625L12.056 12.9887L18.6427 19.5826L19.5847 18.6416L12.997 12.0477L19.5831 5.441Z" fill="#001E50"/>
+                  <path
+                    d="M19.5831 5.441L18.6411 4.5L12.056 11.1067L5.441 4.5L4.5 5.441L11.115 12.0477L4.51579 18.6215L5.45679 19.5625L12.056 12.9887L18.6427 19.5826L19.5847 18.6416L12.997 12.0477L19.5831 5.441Z"
+                    fill="#001E50"
+                  />
                 </svg>
               </button>
             </div>
@@ -70,8 +78,8 @@ const FeedbackContentDialog = (props: FeedbackContentDialogProps) => {
             </div>
             <div class="flex items-center justify-end p-[33px] pt-0 roundedb--[16px]">
               <button
-                type='button'
-                onclick={submit}
+                type="button"
+                onClick={submit}
                 {...props}
                 class={
                   'py-[12px] px-[32px] justify-center font-semibold text-white outline-none flex items-center disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100 transition-all filter hover:brightness-90 active:brightness-75 chatbot-button'
