8098a6bbbb6c358b67df00dc54432ccc03d5c669
Aaron Rackley <aaron.rackley@tribalworldwide.co.uk>
2024-09-30 08:05:00 +0100

    implement feedback dialog styling

--- src/components/FeedbackContentDialog.tsx
+++ src/components/FeedbackContentDialog.tsx
@@ -33,44 +33,28 @@ const FeedbackContentDialog = (props: FeedbackContentDialogProps) => {
       <div class="flex overflow-x-hidden overflow-y-auto fixed inset-0 z-[1002] outline-none focus:outline-none justify-center items-center">
         <div class="relative w-full my-6 max-w-3xl mx-4">
           <div
-            class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+            class="border-0 rounded-[16px] shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
             style={{
               'background-color': props.backgroundColor ?? defaultBackgroundColor,
               color: props.textColor ?? defaultTextColor,
             }}
           >
-            <div
-              class="flex items-center justify-between p-5 border-b border-solid border-blueGray-200 rounded-t"
-              style={{
-                border: '1px solid #eeeeee',
-              }}
-            >
-              <span class="whitespace-pre-wrap font-semibold max-w-full">Provide additional feedback</span>
+            <div class="flex items-center justify-between p-[33px] pb-[24px] rounded-t-[16px]">
+              <span class="whitespace-pre-wrap max-w-full text-[20px]" style={{
+                'font-weight': 300, 
+                'font-family': 'vw-text'
+              }}>Provide additional feedback</span>
               <button
                 class="p-1 ml-auto bg-transparent border-0 text-black float-right text-xl leading-none font-semibold outline-none focus:outline-none"
                 type="button"
                 onClick={onClose}
               >
-                <span class="bg-transparent block outline-none focus:outline-none">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="text-black h-6 w-6"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M18 6 6 18" />
-                    <path d="m6 6 12 12" />
-                  </svg>
-                </span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M19.5831 5.441L18.6411 4.5L12.056 11.1067L5.441 4.5L4.5 5.441L11.115 12.0477L4.51579 18.6215L5.45679 19.5625L12.056 12.9887L18.6427 19.5826L19.5847 18.6416L12.997 12.0477L19.5831 5.441Z" fill="#001E50"/>
+                </svg>
               </button>
             </div>
-            <div class="relative p-6 flex-auto">
+            <div class="relative p-[33px] pt-0 flex-auto">
               <textarea
                 onInput={(e) => handleInput(e.currentTarget.value)}
                 ref={inputRef as HTMLTextAreaElement}
@@ -84,11 +68,18 @@ const FeedbackContentDialog = (props: FeedbackContentDialogProps) => {
                 value={inputValue()}
               />
             </div>
-            <div class="flex items-center justify-end p-4 border-t border-solid border-blueGray-200 rounded-b">
+            <div class="flex items-center justify-end p-[33px] pt-0 roundedb--[16px]">
               <button
-                class="bg-emerald-500 text-white active:bg-emerald-600 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={submit}
+                type='button'
+                onclick={submit}
+                {...props}
+                class={
+                  'py-[12px] px-[32px] justify-center font-semibold text-white outline-none flex items-center disabled:opacity-50 disabled:cursor-not-allowed disabled:brightness-100 transition-all filter hover:brightness-90 active:brightness-75 chatbot-button'
+                }
+                style={{
+                  'border-radius': '100px',
+                  background: '#001E50',
+                }}
               >
                 Submit Feedback
               </button>
