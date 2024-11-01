2be1d6224bdc2a3ed7b169d740684e5a525f097a
Aaron Rackley <aaron.rackley@tribalworldwide.co.uk>
2024-11-01 15:37:58 +0000

    fix: add missing files

--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -50,7 +50,7 @@ export const BotBubble = (props: Props) => {
 
   // stripped out message
   let strippedMessage = props.message.message;
-  strippedMessage = strippedMessage.replace('*command:range-given*', '')
+  strippedMessage = strippedMessage.replace('*command:range-given*', '');
 
   const downloadFile = async (fileAnnotation: any) => {
     try {
@@ -360,31 +360,38 @@ export const BotBubble = (props: Props) => {
           )}
           {props.message.message && (
             <>
-            <div
-              ref={botMessageEl}
-              class="ml-2 max-w-full chatbot-host-bubble prose p-[24px]"
-              data-testid="host-bubble"
-              style={{
-                'background-color': 'white',
-                color: props.textColor ?? defaultTextColor,
-                'border-radius': '16px',
-                'font-size': '16px',
-              }}
-            />
-            {props.message.message.includes('command:range-given') &&
-              <div class='text-sm px-[24px] py-[4px]'>
-                <details>
-                  <summary>
-                    This response has a disclaimer attached for Range specifications. click here to read more.
-                  </summary>
-                  <p class='pt-[4px]'>
-                    *All vehicles are tested according to WLTP technical procedures. Figures shown are for comparability purposes; only compare fuel consumption, CO2 and equivalent electric range figures with other vehicles tested to the same technical procedures. These figures may not reflect real life driving results, which will depend upon a number of factors including the accessories fitted (post-registration), variations in weather, driving styles, speed, vehicle age, vehicle load (and, for battery electric vehicles and plug-in hybrid vehicles, the starting charge of the battery and battery age).
-                    <br /><br />
-                    Figures for plug-in hybrid vehicles were obtained using a combination of battery power and fuel. Plug-in hybrid vehicles require mains electricity for charging. Figures for battery electric vehicles were obtained after the battery had been fully charged. Battery electric vehicles require mains electricity for charging. Zero emissions while driving. Figures quoted are subject to change due to ongoing approvals/changes. Please consult your retailer for further information and range figures may include options not available in the UK. Data correct at March 2024.
-                  </p>
-                </details>
-              </div>
-            }
+              <div
+                ref={botMessageEl}
+                class="ml-2 max-w-full chatbot-host-bubble prose p-[24px]"
+                data-testid="host-bubble"
+                style={{
+                  'background-color': 'white',
+                  color: props.textColor ?? defaultTextColor,
+                  'border-radius': '16px',
+                  'font-size': '16px',
+                }}
+              />
+              {props.message.message.includes('command:range-given') && (
+                <div class="text-sm px-[24px] py-[4px]">
+                  <details>
+                    <summary>This response has a disclaimer attached for Range specifications. click here to read more.</summary>
+                    <p class="pt-[4px]">
+                      *All vehicles are tested according to WLTP technical procedures. Figures shown are for comparability purposes; only compare fuel
+                      consumption, CO2 and equivalent electric range figures with other vehicles tested to the same technical procedures. These
+                      figures may not reflect real life driving results, which will depend upon a number of factors including the accessories fitted
+                      (post-registration), variations in weather, driving styles, speed, vehicle age, vehicle load (and, for battery electric vehicles
+                      and plug-in hybrid vehicles, the starting charge of the battery and battery age).
+                      <br />
+                      <br />
+                      Figures for plug-in hybrid vehicles were obtained using a combination of battery power and fuel. Plug-in hybrid vehicles require
+                      mains electricity for charging. Figures for battery electric vehicles were obtained after the battery had been fully charged.
+                      Battery electric vehicles require mains electricity for charging. Zero emissions while driving. Figures quoted are subject to
+                      change due to ongoing approvals/changes. Please consult your retailer for further information and range figures may include
+                      options not available in the UK. Data correct at March 2024.
+                    </p>
+                  </details>
+                </div>
+              )}
             </>
           )}
           {props.message.action && (
