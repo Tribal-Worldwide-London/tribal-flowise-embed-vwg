bc1874537487cb0511d185c318ae9939762cb95b
Aaron Rackley <aaron.rackley@tribalworldwide.co.uk>
2024-11-04 13:51:34 +0000

    feature: added in more disclaimers.

---
+++ dist/components/disclaimers/DisclaimerContainer.d.ts
@@ -0,0 +1,6 @@
+type Props = {
+    message: string;
+};
+export declare const DisclaimerContainer: (props: Props) => import("solid-js").JSX.Element;
+export default DisclaimerContainer;
+//# sourceMappingURL=DisclaimerContainer.d.ts.map
--- src/components/bubbles/BotBubble.tsx
+++ src/components/bubbles/BotBubble.tsx
@@ -8,6 +8,9 @@ import FeedbackContentDialog from '../FeedbackContentDialog';
 import { AgentReasoningBubble } from './AgentReasoningBubble';
 import { TickIcon, XIcon } from '../icons';
 import { sendAnalyticsEvent } from '@/utils/analytics';
+import RangeDisclaimer from '../disclaimers/RangeDisclaimer';
+import ChargeDisclaimer from '../disclaimers/ChargeDisclaimer';
+import DisclaimerContainer from '../disclaimers/DisclaimerContainer';
 
 type Props = {
   message: MessageType;
@@ -48,9 +51,12 @@ export const BotBubble = (props: Props) => {
   const [thumbsUpColor, setThumbsUpColor] = createSignal(props.feedbackColor ?? defaultFeedbackColor); // default color
   const [thumbsDownColor, setThumbsDownColor] = createSignal(props.feedbackColor ?? defaultFeedbackColor); // default color
 
-  // stripped out message
+  // stripped out message to remove commands
   let strippedMessage = props.message.message;
-  strippedMessage = strippedMessage.replace('*command:range-given*', '');
+  if(strippedMessage){
+    strippedMessage = strippedMessage.replace('*command:range-given*', '');
+    strippedMessage = strippedMessage.replace('*command:charge-given*', '');
+  }
 
   const downloadFile = async (fileAnnotation: any) => {
     try {
@@ -371,30 +377,7 @@ export const BotBubble = (props: Props) => {
                   'font-size': '16px',
                 }}
               />
-              {props.message.message.includes('command:range-given') && (
-                <div class="text-sm px-[24px] py-[4px]">
-                  <details>
-                    <summary>Note: This response includes a disclaimer about estimated charging times. Tap here for details.</summary>
-                    <p class="pt-[4px]">
-                      Charging times will vary depending on various factors, including the selected vehicle, standard specification, the options you
-                      choose, the type of charger used, the level of charge in the battery, the age type, condition and temperature of the charger and
-                      the battery, the power supply, ambient temperature at the point of use and other environmental factors.
-                      <br />
-                      <br />
-                      Charging time will be longer in cold weather. Charging times will also be affected by the charging curve (for example, once
-                      charging passes 80%, charging will slow to protect the battery's longevity) and will be longer if battery temperature activates
-                      safeguarding technology. Figures are subject to change due to ongoing approvals. The availability of 150kWh and 350kWh chargers
-                      is currently limited in the UK. They are mainly located on selected motorways and major arterial routes. These chargers are not
-                      currently available in Northern Ireland and numbers are extremely low in Scotland, Wales, and parts of England. Please see EV
-                      Route Planner for charging points available in the UK:
-                      <a href="https://www.volkswagen.co.uk/en/electric-and-hybrid/living-electric/lifestyle/ev-route-planner.html" target="_blank">
-                        {' '}
-                        https://www.volkswagen.co.uk/en/electric-and-hybrid/living-electric/lifestyle/ev-route-planner.html
-                      </a>
-                    </p>
-                  </details>
-                </div>
-              )}
+              <DisclaimerContainer message={props.message.message} />
             </>
           )}
           {props.message.action && (
